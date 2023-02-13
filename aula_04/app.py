ab64aecec20ac21e34ee4040e67d116dde91ae5e
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2023-02-13 00:01:56 -0300

    feat(aula inicial) - quinta aula - Carregando arquivos estáticos

--- aula_04/app.py
+++ aula_04/app.py
@@ -1,29 +1,7 @@
-# Aula 04 - COntrução de URLs
-from flask import Flask, redirect, url_for
+# Aula 4 - Arquivos estáticos
+from flask import Flask
 
-app = Flask(__name__)
-
-@app.route("/admin/")
-@app.route("/admin/<name>")
-def admin(name = ""):
-    if(not name):
-        return "<h1>Olá, administrador</h1>"
-    return "<p>Olá, {}. Você está logado como administrador.".format(name)
-
-@app.route("/guest/")
-@app.route("/guest/<name>")
-def guest(name = ""):
-    if(not name):
-        return "<p>Olá, visitante</p>"
-    return "<p>Olá, {}. Você está logado como visitante.".format(name)
-
-@app.route("/user/")
-@app.route("/user/<name>")
-def user(name = ""):
-    if name == "admin":
-        return redirect(url_for("admin"))
-    return redirect(url_for("guest"))
-
+app = Flask(__name__, static_folder='site')
 
 if __name__ == '__main__':
-        app.run(debug=True)
+    app.run(debug=True)
